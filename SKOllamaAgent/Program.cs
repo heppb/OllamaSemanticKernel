8fdc609605eeab0a54bbffd75bf3ae880bd5fe51
heppb <22299591+heppb@users.noreply.github.com>
2025-05-20 17:34:15 -0400

    Initial Ollama 3.2 Semantic Kernel Setup

---
+++ SKOllamaAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Kernel.CreateBuilder();
+// 👇🏼 Using Phi-4 with Ollama
+#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+builder.AddOllamaChatCompletion("llama3.2:latest", new Uri("http://localhost:11434"));
+#pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+var kernel = builder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = [];
+history.AddUserMessage("Hello, how are you? Can you tell me an appropriate joke?");
+
+var response = chatCompletionService.GetStreamingChatMessageContentsAsync(
+    chatHistory: history,
+    kernel: kernel
+);
+
+await foreach (var chunk in response)
+{
+    Console.Write(chunk);
+}
+
